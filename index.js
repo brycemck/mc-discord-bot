7305832829c99c8cc043bffe5c5b7b94286d230e
Bryce McKenney <brycemckenney@gmail.com>
2021-04-20 02:42:14 -0700

    added commands

--- index.js
+++ index.js
@@ -3,6 +3,23 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const TOKEN = process.env.TOKEN;
 
+function notAnAdmin(msg) {
+  msg.channel.send("you are not an admin!")
+}
+function notACommand(msg) {
+  msg.channel.send("That's not a command homie")
+}
+function status(msg) {
+  msg.channel.send("status check")
+}
+
+let commands = new Map();
+commands.set("status", status);
+
+let admins = [ // ids of users
+  "635323524713807914"
+]
+
 bot.login(TOKEN);
 
 bot.on('ready', () => {
@@ -10,16 +27,32 @@ bot.on('ready', () => {
 });
 
 bot.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-    msg.channel.send('pong');
-
-  } else if (msg.content.startsWith('!kick')) {
-    if (msg.mentions.users.size) {
-      const taggedUser = msg.mentions.users.first();
-      msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
+  console.info(msg.content)
+  // console.log(msg)
+ if (msg.content[0] === '!') {
+  if (admins.includes(msg.author.id)) {
+    console.log("passed admin check")
+    const command = msg.content.split(" ")[0].substr(1);
+    if (commands.has(command)) {
+      console.log(command + " is a command.")
+      commands.get(command)(msg)
     } else {
-      msg.reply('Please tag a valid user!');
+      notACommand(msg);
     }
+  } else {
+    notAnAdmin(msg);
   }
+ }
+  // if (msg.content === 'ping') {
+  //   msg.reply('pong');
+  //   msg.channel.send('pong');
+
+  // } else if (msg.content.startsWith('!kick')) {
+  //   if (msg.mentions.users.size) {
+  //     const taggedUser = msg.mentions.users.first();
+  //     msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
+  //   } else {
+  //     msg.reply('Please tag a valid user!');
+  //   }
+  // }
 });
